19f1a8ab7c7ede521901ce590c72d724037b04ae
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-09-02 19:44:21 -0300

    modulo 3 unidad 4 tarea punto 3 terminada

--- Modulo 3/Unidad 4 Javascript pt2/tarea/app.js
+++ Modulo 3/Unidad 4 Javascript pt2/tarea/app.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    //Punto 1
     const btnRed = document.getElementsByClassName('btnRed')[0];
     const btnBlue = document.getElementsByClassName('btnBlue')[0];
     const btnGreen = document.getElementsByClassName('btnGreen')[0];
@@ -19,10 +20,71 @@ document.addEventListener('DOMContentLoaded', function() {
         body.style.backgroundColor = '#00ff00';
     }, false);
 
-
+    //Punto 2
     const textarea = document.getElementsByClassName('textarea')[0];
     const textareaTam = document.getElementsByClassName('textareaTam')[0];
     textarea.addEventListener('change', function(){
         textareaTam.innerHTML = '<strong> El tamaño del textarea es de: ' + textarea.value.length + '</strong>';
     });
 });
+
+//Punto 3
+var alumnos = [
+
+  {
+
+    nombre: 'Juan Gomez',
+
+    nota: 7
+
+  }, {
+
+    nombre: 'Pedro Rodriguez',
+
+    nota: 4
+
+  }, {
+
+    nombre: 'Roxana García',
+
+    nota: 8
+
+  }, {
+
+    nombre: 'Luciano Lopez',
+
+    nota: 5
+
+  }, {
+
+    nombre: 'Fernanda Gimenez',
+
+    nota: 6
+
+  }, {
+
+    nombre: 'Florencia Martinez',
+
+    nota: 10
+
+  }, {
+
+    nombre: 'Raul Sanchez',
+
+    nota: 7
+
+  }, {
+
+    nombre: 'Sandra Figueroa',
+
+    nota: 8
+
+  }
+
+];
+
+const aprobados = alumnos.filter(function(alumno){
+    return alumno.nota >= 7;
+});
+
+console.log(aprobados);
